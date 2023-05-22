c8fb5330c6da7103ee56a1d4d3d198b47b397180
Christopher Morrison <Chris@Tests-MacBook-Pro.local>
2023-05-22 12:16:37 -0600

    Made some progress.

---
+++ lib/kv/bucket.ex
@@ -0,0 +1,36 @@
+defmodule KV.Bucket do
+  use Agent
+
+  @doc """
+  Starts a new bucket.
+  """
+  def start_link(_opts) do
+    Agent.start_link(fn -> %{} end)
+  end
+
+  @doc """
+  Gets a value from the 'bucket' by 'key'.
+  &Map.get(&1, key) == fn(map) -> Map.get(map, key)
+  """
+  def get(bucket, key) do
+    Agent.get(bucket, &Map.get(&1, key))
+  end
+
+  @doc """
+  Puts the 'value' for the given 'key' in the 'bucket'.
+  &Map.put(&1, key, value) == fn(map) -> Map.put(map, key, value)
+  """
+  def put(bucket, key, value) do
+    Agent.update(bucket, &Map.put(&1, key, value))
+  end
+
+  @doc """
+  Deletes 'key' from 'bucket'.any()
+
+  Returns the current value of 'key', if 'key' exists.
+  """
+  def delete(bucket, key) do
+    Agent.get_and_update(bucket, &Map.pop(&1, key))
+  end
+
+end
